44b749fd4f350f01a24854902434ca602a31574d
Okuto Oyama <0910yama@gmail.com>
2022-03-27 20:51:50 +0900

    md 内リンクを外部リンク処理扱いにする (#4)

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,18 @@
+const markdownIt = require("markdown-it");
+const mila = require("markdown-it-link-attributes");
+
 module.exports = (eleventyConfig) => {
+  const mdOptions = {
+    html: true
+  };
+  const milaOptions = {
+    attrs: {
+      target: "_blank",
+      rel: "noopener"
+    }
+  };
+  const markdownLib = markdownIt(mdOptions).use(mila, milaOptions);
+  eleventyConfig.setLibrary("md", markdownLib);
   eleventyConfig.addPassthroughCopy("images");
   return {
     dir: {
